19688d6653c0b14a4cf46b5355d21c1695de51cd
ryanjamessmith1978 <ryan.james.smith.1978@gmail.com>
2025-01-08 17:24:27 -0500

    Add files via upload

---
+++ Projects/InterviewQuestions_test/InterviewQuestions_test/btree.h
@@ -0,0 +1,29 @@
+#pragma once
+struct node
+{
+	int key_value;
+	node* left;
+	node* right;
+};
+
+class btree
+{
+public:
+	btree();
+	~btree();
+
+	void insert(int key);
+	node *search(int key);
+	void destroy_tree();
+
+private:
+
+	void destroy_tree(node* leaf);
+	void insert(int key, node* leaf);
+	node* search(int key, node* leaf);
+
+	node* root;
+	int myRoot = 0;
+	int& myroot;
+};
+
