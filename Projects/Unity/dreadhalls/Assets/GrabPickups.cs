625e8433fc1b8f1e1c042c1423866a7ceb44ee9b
ryanjamessmith1978 <ryan.james.smith.1978@gmail.com>
2025-01-10 12:43:18 -0500

    reAdd of dreadhalls

---
+++ Projects/Unity/dreadhalls/Assets/GrabPickups.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class GrabPickups : MonoBehaviour {
+	
+	private AudioSource pickupSoundSource;
+	public Text levelText;
+	public LevelGenerator generator;
+
+	void Awake() {
+		pickupSoundSource = DontDestroy.instance.GetComponents<AudioSource>()[1];
+		levelText.text = "Level " + generator.returnLevel();
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		if (hit.gameObject.tag == "Pickup") {
+			generator.addLevel();
+			pickupSoundSource.Play();
+			SceneManager.LoadScene("Play");
+		}
+	}
+}
---
+++ Projects/Unity/dreadhalls/Assets/Scripts/GameOverTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class GameOverTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	public LevelGenerator generator;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		generator.resetLevel();
+		SceneManager.LoadScene("GameOver");
+	}
+
+	// 
+}
