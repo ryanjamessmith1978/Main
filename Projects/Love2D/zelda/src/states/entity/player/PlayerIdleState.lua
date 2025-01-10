c704f8d20b8c549f95720ab3e270743aba1b7c51
ryanjamessmith1978 <ryan.james.smith.1978@gmail.com>
2025-01-09 20:09:31 -0500

    Add files via upload

---
+++ Projects/Love2D/zelda/src/states/entity/player/PlayerIdleState.lua
@@ -0,0 +1,32 @@
+--[[
+    GD50
+    Legend of Zelda
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+PlayerIdleState = Class{__includes = EntityIdleState}
+
+function PlayerIdleState:enter(params)
+    
+    -- render offset for spaced character sprite (negated in render function of state)
+    self.entity.offsetY = 5
+    self.entity.offsetX = 0
+end
+
+function PlayerIdleState:update(dt)
+    if love.keyboard.isDown('left') or love.keyboard.isDown('right') or
+       love.keyboard.isDown('up') or love.keyboard.isDown('down') then
+        self.entity:changeState('walk')
+    end
+
+    if love.keyboard.wasPressed('space') and self.entity.canSwing == true then
+        self.entity:changeState('swing-sword')
+    elseif love.keyboard.wasPressed('space') and self.entity.potAttached == true then
+        -- do something
+        self.entity.potAttached = false
+        self.entity.canSwing = true
+    end
+
+end
