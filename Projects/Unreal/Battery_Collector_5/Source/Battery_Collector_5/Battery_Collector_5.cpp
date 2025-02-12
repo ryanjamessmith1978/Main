ec0b3da9945d6922b27eb315ac1db8a40e5c59a7
ryanjamessmith1978 <ryan.james.smith.1978@gmail.com>
2025-02-12 15:59:06 -0500

    More BC cleanup

--- Projects/Unreal/Battery_Collector_5/Source/Battery_Collector_5/Battery_Collector_5.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Battery_Collector_5.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Battery_Collector_5, "Battery_Collector_5" );
- 
