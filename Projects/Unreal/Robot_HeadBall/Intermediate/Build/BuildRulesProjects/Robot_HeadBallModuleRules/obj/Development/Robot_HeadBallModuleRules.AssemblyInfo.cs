5d09c3dd712db45c7834d36ef33aafdcd1a4994e
ryanjamessmith1978 <ryan.james.smith.1978@gmail.com>
2025-03-24 11:03:35 -0400

    Basic Arena complete

--- Projects/Unreal/Robot_HeadBall/Intermediate/Build/BuildRulesProjects/Robot_HeadBallModuleRules/obj/Development/Robot_HeadBallModuleRules.AssemblyInfo.cs
+++ Projects/Unreal/Robot_HeadBall/Intermediate/Build/BuildRulesProjects/Robot_HeadBallModuleRules/obj/Development/Robot_HeadBallModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Robot_HeadBallModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c8cebbe2e800453122008496cc11674135ff5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e33a0704a64a1cd12442ccf81aa8be60f5308a34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Robot_HeadBallModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Robot_HeadBallModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
