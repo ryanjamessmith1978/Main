6dc664b266cceb80e7a55c7a25161eee0240e3c8
ryanjamessmith1978 <ryan.james.smith.1978@gmail.com>
2025-04-08 18:44:30 -0400

    Updated Player pickup mechanic, added UI scoreboard, project folder cleanup

--- Projects/Unreal/Robot_HeadBall/Source/Robot_HeadBall/Robot_HeadBall.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Robot_HeadBall.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Robot_HeadBall, "Robot_HeadBall" );
- 
