dd2b1853aed4bd2c03655903e3a953636167a7bc
ryanjamessmith1978 <ryan.james.smith.1978@gmail.com>
2025-04-09 20:25:46 -0400

    updated character throw mechanic, includes spline mesh curve

--- Projects/Unreal/Robots_HeadBall/Intermediate/Build/Win64/x64/ShaderCompileWorker/Development/PostBuild-1.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo Copying C:\UnrealEngine\Engine\Binaries\Win64\ShaderCompileWorker.exe to C:\UnrealEngine\Engine\Binaries\Win64\XGEControlWorker.exe
-copy /Y /B "C:\UnrealEngine\Engine\Binaries\Win64\ShaderCompileWorker.exe" /B "C:\UnrealEngine\Engine\Binaries\Win64\XGEControlWorker.exe" >nul:
